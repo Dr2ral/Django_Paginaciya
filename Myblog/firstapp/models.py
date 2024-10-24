2d58ecb2a2eab1ed3687933d93ca64cd8a7b6087
Dr2ral <hobbygame90@gmail.com>
2024-10-24 20:19:45 +0300

    выпад_список

--- Myblog/firstapp/models.py
+++ Myblog/firstapp/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-# Create your models here.
-#class Post(models.Model):
-#    title = models.CharField(max_length=100)
-#    content = models.TextField()
-#    created_at = models.DateTimeField(auto_now_add=True)
-
 class Game(models.Model):
     title = models.CharField(max_length=30)
     cost = models.DecimalField(decimal_places=1, max_digits=5, blank=False)
--- Myblog/firstapp/views.py
+++ Myblog/firstapp/views.py
@@ -6,11 +6,29 @@ from django.core.paginator import Paginator
 # Create your views here.
 def game(request):
     posts = Game.objects.all().order_by('title')
-    paginator = Paginator(posts, 2)
     page_number = request.GET.get('page')
+    pages = request.GET.get('pages', 2)
+    paginator = Paginator(posts, pages)
     page_obj = paginator.get_page(page_number)
+
+
     context = {
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'pages': pages
     }
     return render(request, 'index.html', context)
 
+
+#def choose_elem(request):
+#    games = Game.objects.all()
+#    page = request.GET.get('page', 1)
+#    paginator = Paginator(games, page)
+#
+#    page_number = request.GET.get('page')
+#    page_obj = paginator.get_page(page_number)
+#
+#    context = {
+#        'page_obj': page_obj,
+#        'page': page,
+#    }
+#    return render(request, 'index.html', context)
